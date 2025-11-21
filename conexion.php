54784ab981c9a926ec12474c768a22aa54fa9687
montesmilna27-max <montesmilna27@autonoma.edu.pe>
2025-11-20 22:26:59 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
-$host = "localhost";
-$dbname = "vet_citas";
-$user = "root";
-$pass = "";
+require_once __DIR__ . '/config.local.php';
 
 try {
-    $con = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $con = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8",
+        DB_USER,
+        DB_PASS
+    );
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
-?>
